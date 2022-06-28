a17969327078c1b1f7f691785f9a79ce28cf1aca
ROSS LEE <Rosslee@wicklowvec.ie>
2022-06-28 14:00:52 +0100

    bifesound

---
+++ data.js
@@ -0,0 +1,633 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sound-department-entrance",
+      "name": "Sound Department Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 2.8518745445424596,
+        "pitch": -0.01198335260765404,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.230238204348664,
+          "pitch": 0.2213489512536082,
+          "rotation": 0,
+          "target": "1-recording-studio"
+        },
+        {
+          "yaw": 2.6476563828215856,
+          "pitch": 0.14524938534790977,
+          "rotation": 0,
+          "target": "5-music-production-studio"
+        },
+        {
+          "yaw": 3.098733691223851,
+          "pitch": 0.07612748289045079,
+          "rotation": 0,
+          "target": "6-dj-studio"
+        },
+        {
+          "yaw": -0.34643988566849515,
+          "pitch": 0.14550083638810918,
+          "rotation": 0,
+          "target": "2-canteen-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-recording-studio",
+      "name": "Recording Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26249110038506096,
+          "pitch": -0.045004399821269914,
+          "rotation": 0,
+          "target": "4-rehearsal-studio"
+        },
+        {
+          "yaw": 2.828730740926731,
+          "pitch": 0.024422589952182605,
+          "rotation": 0,
+          "target": "0-sound-department-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22390456905115563,
+          "pitch": 0.11480191952610852,
+          "title": "Sound Desk",
+          "text": "BIFE have a fully equipped state of the art recording facility"
+        }
+      ]
+    },
+    {
+      "id": "2-canteen-1",
+      "name": "Canteen 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -1.7475454537604183,
+        "pitch": 0.04752785840703133,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9028657582831787,
+          "pitch": 0.01669388917764536,
+          "rotation": 0,
+          "target": "0-sound-department-entrance"
+        },
+        {
+          "yaw": 2.943121664333855,
+          "pitch": 0.012002675039614275,
+          "rotation": 0,
+          "target": "8-main-college-entrance"
+        },
+        {
+          "yaw": -2.6721209744482195,
+          "pitch": -0.003213493114543553,
+          "rotation": 0,
+          "target": "11-courtyard"
+        },
+        {
+          "yaw": -1.2203350732317748,
+          "pitch": -0.017342340743820017,
+          "rotation": 0,
+          "target": "7-canteen-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8671883747067035,
+          "pitch": -0.1045496004842228,
+          "title": "Music Department",
+          "text": "Access to Recording Studio A, Studio Lab B and DJ Studio C including DJ booths"
+        },
+        {
+          "yaw": 2.4609802697436702,
+          "pitch": -0.09892646452230025,
+          "title": "Canteen",
+          "text": "Hot &amp; Cold food served daily"
+        }
+      ]
+    },
+    {
+      "id": "3-bbox1",
+      "name": "BBox1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-rehearsal-studio",
+      "name": "Rehearsal Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.2580322665987733,
+        "pitch": 0.06584500421797301,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5726264323024424,
+          "pitch": -0.026012018047742558,
+          "rotation": 0,
+          "target": "7-canteen-2"
+        },
+        {
+          "yaw": -2.7299543427543878,
+          "pitch": -0.0006917720357400015,
+          "rotation": 0,
+          "target": "1-recording-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-music-production-studio",
+      "name": "Music Production Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.77733305731984,
+        "pitch": -0.06323883604970248,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.716936371567698,
+          "pitch": 0.08212029025488121,
+          "rotation": 0,
+          "target": "0-sound-department-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dj-studio",
+      "name": "DJ Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -3.040003598155847,
+        "pitch": 0.006142562159780596,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2269580689231656,
+          "pitch": -0.00951236066238792,
+          "rotation": 0,
+          "target": "0-sound-department-entrance"
+        },
+        {
+          "yaw": 0.039637562812920635,
+          "pitch": 0.10274296436422858,
+          "rotation": 0,
+          "target": "10-dj-booths"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.12425413866321477,
+          "pitch": 0.2691253982979145,
+          "title": "DJ Booths Access",
+          "text": "6 fully equipped DJ booths"
+        }
+      ]
+    },
+    {
+      "id": "7-canteen-2",
+      "name": "Canteen 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.9970149339504317,
+        "pitch": -0.09700368162054573,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6962825548319813,
+          "pitch": 0.014531314226552183,
+          "rotation": 0,
+          "target": "4-rehearsal-studio"
+        },
+        {
+          "yaw": 0.23756587185499,
+          "pitch": -0.006671706581842685,
+          "rotation": 0,
+          "target": "2-canteen-1"
+        },
+        {
+          "yaw": 2.1396627022736316,
+          "pitch": 0.053810889084616065,
+          "rotation": 0,
+          "target": "8-main-college-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.063045160963794,
+          "pitch": -0.26483588144138004,
+          "title": "Performance Stage",
+          "text": "BIFE students perform in dance, theatre and music performance shows throughout the year"
+        }
+      ]
+    },
+    {
+      "id": "8-main-college-entrance",
+      "name": "Main College Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 3.008733595094773,
+        "pitch": 0.024726428648373044,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8141929425706778,
+          "pitch": 0.08419253961124795,
+          "rotation": 0,
+          "target": "7-canteen-2"
+        },
+        {
+          "yaw": -1.814931774133731,
+          "pitch": 0.05456672108418559,
+          "rotation": 0,
+          "target": "2-canteen-1"
+        },
+        {
+          "yaw": 2.952723210461979,
+          "pitch": 0.2919591359240563,
+          "rotation": 0,
+          "target": "11-courtyard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Exit",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -1.5963398580126924,
+          "pitch": 0.08629135921077058,
+          "title": "College Reception",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "9-dj-booth",
+      "name": "DJ Booth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -3.095985604498031,
+        "pitch": 0.1258323756951949,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.463235800243093,
+          "pitch": 0.12470081440801906,
+          "rotation": 0,
+          "target": "10-dj-booths"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-dj-booths",
+      "name": "DJ Booths",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -1.498194527786124,
+        "pitch": 0.028906410450630915,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0213824174258495,
+          "pitch": 0.059507019611203305,
+          "rotation": 0,
+          "target": "6-dj-studio"
+        },
+        {
+          "yaw": -1.16796317752598,
+          "pitch": 0.35052030017224567,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        },
+        {
+          "yaw": -2.389583672879887,
+          "pitch": 0.22440120796110286,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        },
+        {
+          "yaw": -0.42683943082968767,
+          "pitch": 0.32366499671426396,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        },
+        {
+          "yaw": 0.49950719309379643,
+          "pitch": 0.31131182565863824,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        },
+        {
+          "yaw": 1.1902273152081282,
+          "pitch": 0.5019026016382853,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        },
+        {
+          "yaw": 2.7440292923063385,
+          "pitch": 0.32400220127186863,
+          "rotation": 0,
+          "target": "9-dj-booth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-courtyard",
+      "name": "Courtyard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 2.8094592497302138,
+        "pitch": -0.17338511606752327,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38043596011866576,
+          "pitch": 0.11199401015026211,
+          "rotation": 0,
+          "target": "8-main-college-entrance"
+        },
+        {
+          "yaw": 3.113220236708912,
+          "pitch": 0.03295402937213865,
+          "rotation": 0,
+          "target": "2-canteen-1"
+        },
+        {
+          "yaw": 2.222069732935239,
+          "pitch": 0.017476245498937004,
+          "rotation": 0,
+          "target": "7-canteen-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
